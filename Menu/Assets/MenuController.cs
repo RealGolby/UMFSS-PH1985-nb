c0964522b4703fb6d1f8ab6fc80bed090e2b0afe
RealGolby <73080347+RealGolby@users.noreply.github.com>
2021-12-08 11:45:20 +0100

    1.1

---
+++ Menu/Assets/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] GameObject Menu;
+    [SerializeField] GameObject Player;
+
+    public void OpenMenu()
+    {
+        Menu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToGame()
+    {
+        Menu.SetActive(false);
+    }
+
+    public void RespawnPlayer()
+    {
+        Player.transform.position = new Vector3();
+    }
+}
